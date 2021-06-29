d81294b42fce10651e5b6ea45e83471a13ff95ef
Salah Eddin Boukair <83084081+salah137@users.noreply.github.com>
2021-06-29 16:18:51 +0100

    I Update My Files

--- main/lib/screens/MarketScreen.dart
+++ main/lib/screens/MarketScreen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
-class ChatsScreen extends StatelessWidget {
+class MarketScreen extends StatelessWidget {
+  const MarketScreen({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      
     );
   }
 }
